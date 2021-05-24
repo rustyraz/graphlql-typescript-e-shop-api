d5540f71dc282cbe04cd33d89c09d56db8fea824
Peter Ngerere <pngesh@gmail.com>
2021-05-24 09:42:05 +0300

    clean up

--- dist/schema.ts
+++
@@ -1,9 +0,0 @@
-import { makeExecutableSchema } from 'graphql-tools';
-import { ServiceTypeDefs } from './service/serviceSchema';
-import serviceResolver from './service/serviceResolver';
-
-export const schema = makeExecutableSchema({
-    typeDefs: ServiceTypeDefs,
-    resolvers: serviceResolver
-});
-
